4a6ef34d3e5b720a64e5641d1d0b2eb7f3c12869
hibiki esaki <61895243+hibiki0612@users.noreply.github.com>
2021-10-04 00:11:46 +0900

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -222,26 +222,5 @@ def main():
                    
 
 
-'''
-                if len(hands) == 2:
-                    # Hand 2
-                    hand2 = hands[1]
-                    lmList2 = hand2["lmList"]  # List of 21 Landmark points
-                    bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
-                    centerPoint2 = hand2['center']  # center of the hand cx,cy
-                    handType2 = hand2["type"]  # Hand Type "Left" or "Right"
-
-                    fingers2 = detector.fingersUp(hand2)
-
-                    # Find Distance between two Landmarks. Could be same hand or different hands
-                    length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
-                    print(length)
-                    # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
-            # Display
-    
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-'''
-
 if __name__ == "__main__":
     main()
